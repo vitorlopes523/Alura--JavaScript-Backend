181c6c4d28b69d8f71af2d81afd5f52b9665d17e
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2023-06-07 11:34:20 -0400

    adicionei conteúdo da aula de variáveis

---
+++ JavaScript-tipos-variaveis-funcoes/Variaveis/conversoes.js
@@ -0,0 +1,16 @@
+//tipo de dado
+//booleanos
+
+//conversão implícita
+const numero = 456
+const numeroString = "456"
+
+console.log(numero == numeroString)
+console.log(numero + numeroString)//concatenação
+
+//conversão explícita
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+console.log(String(numero) + numeroString)
+
