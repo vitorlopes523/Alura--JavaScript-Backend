07e22fc666f989109c637fe8f14b6ae81b794dae
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2023-07-19 11:36:57 -0400

    adicionei conteúdo das aulas de criação de biblioteca

--- NodeJS-criando-sua-primeira-biblioteca/criando-biblioteca/index.js
+++ NodeJS-criando-sua-primeira-biblioteca/criando-biblioteca/index.js
@@ -1,9 +1,41 @@
+import fs from 'fs'
 import chalk from 'chalk'
 
-console.log(chalk.blue('olá mundo'))
+function extrairLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)]
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados
+}
 
-console.log('olá mundo');
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'))
+}
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+// async/await
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const enconding = 'utf-8'
+    const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+    console.log(extrairLinks(chalk.blue(texto)))
+  } catch (erro) {
+    trataErro(erro)
+  } finally {
+    console.log(chalk.yellow('operação concluída'))
+  }
+}
+
+// promises com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const enconding = 'utf-8'
+//   fs.promises
+//     .readFile(caminhoDoArquivo, enconding)
+//     .then((texto) => console.log(chalk.yellow(texto)))
+//     .catch(trataErro)
+// }
+
+
+pegaArquivo('./arquivos/texto.md')
+
